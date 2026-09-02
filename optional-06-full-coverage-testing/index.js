a08d2706b57faff37ca6f5e6c6f6bb91b911a87e
RezaHarahap <114418673+RezaHarahap@users.noreply.github.com>
2026-09-03 01:57:02 +0700

    Tambahkan fungsi full coverage testing

---
+++ optional-06-full-coverage-testing/index.js
@@ -0,0 +1,13 @@
+function sum(a, b) {
+  if (typeof a !== 'number' || typeof b !== 'number') {
+    return 0;
+  }
+
+  if (a < 0 || b < 0) {
+    return 0;
+  }
+
+  return a + b;
+}
+
+export default sum;
